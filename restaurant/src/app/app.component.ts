96e898896f8a4d165c9353f7d67635bd6f2b75a0
Varshini P S <psvarshini34@gmail.com>
2021-05-16 19:36:34 +0530

    few changes added

--- restaurant/src/app/app.component.ts
+++ restaurant/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'restaurant';
 
   routes=[{linkName:'admin',url:'admin'},
 {linkName:'user',url:'user'}];
+
+constructor(){
+  
+}
+
+ngOnInit(){
+
+}
+url="./assets/rest.jpg";
 }
